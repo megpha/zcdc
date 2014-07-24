8bf64e30c7d7ff1973909ec6f86d4c884e061178
Megpha <megpha@yahoo.com>
2014-07-23 21:15:28 -0400

    asset precompilation

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Zcdc
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.assets.paths << Rails.root.join("app", "assets", "components")
+    config.server_static_assets = true
   end
 end
