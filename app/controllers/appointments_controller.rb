2a0d5fcba1df051d6b1a42a2b94afc7c643725a6
Megpha <megpha@yahoo.com>
2014-07-26 10:58:39 -0400

    fixing appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -79,7 +79,7 @@ class AppointmentsController < ApplicationController
   def completed
     @appointment.completed!
     respond_to do |format|
-      format.html { redirect_to today_appointments_path(type: 'active'), notice: 'Appointment was successfully removed.' }
+      format.html { redirect_to today_appointments_path(type: 'active'), notice: 'Appointment was successfully completed.' }
       format.json { head :no_content }
     end
   end
